de59fb81f6c8885b22d346d144cfd593c155f28b
Ali <ali62n62@yahoo.com>
2017-07-12 00:21:07 +0430

    page 107

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace LanguageFeatures.Controllers
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
             decimal cartTotal = cart.TotalPrices();
             return View("Index", new string[] { $"Total: {cartTotal:C2}" });
+            //page 107 in the book
         }
     }
 }
